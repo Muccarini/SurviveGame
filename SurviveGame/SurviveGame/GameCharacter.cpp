2653b7e5beda0be9e8088b038c1cf614166b6aad
Fedelli98 <50577589+Fedelli98@users.noreply.github.com>
2019-07-30 10:45:23 +0200

    inseriti i frame di WASD

--- SurviveGame/SurviveGame/GameCharacter.cpp
+++ SurviveGame/SurviveGame/GameCharacter.cpp
@@ -17,13 +17,25 @@ void GameCharacter::move(sf::Time deltaTime,const float dir_x,const float dir_y)
 void GameCharacter::update(sf::Time deltaTime) 
 {
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+	{
 		move(deltaTime, 0.f, -1.f);
+		_sprite.setTextureRect(sf::IntRect(0 * 32, 48, 32, 48));
+	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+	{
 		move(deltaTime, -1.f, 0.f);
+		_sprite.setTextureRect(sf::IntRect(0 * 32, 48 *2, 32, 48));
+	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+	{
 		move(deltaTime, 0.f, 1.f);
+		_sprite.setTextureRect(sf::IntRect(0 * 32, 0, 32, 48));
+	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	{
 		move(deltaTime, 1.f, 0.f);
+		_sprite.setTextureRect(sf::IntRect(0 * 32, 48 * 3, 32, 48));
+	}
 }
 
 void GameCharacter::render(sf::RenderWindow* target)
