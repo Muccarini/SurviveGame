e4bd6af091b3b00f955ff9b237b3f2d24c5e2b22
Fedelli98 <50577589+Fedelli98@users.noreply.github.com>
2020-03-21 18:01:58 +0100

    +

--- SurviveGame/SurviveGame/Main.cpp
+++ SurviveGame/SurviveGame/Main.cpp
@@ -1,13 +1,9 @@
-#define _CRTDBG_MAP_ALLOC
 #include "Game.h"
-#include <crtdbg.h>
 
 int main()
 {
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	Game game;
 	game.run();
 
-	_CrtDumpMemoryLeaks();
 	return 0;
 }
